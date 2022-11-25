17713d6d319525139bde8ba543df1f20f4c8941b
Georgi Raynov <jornalo891@gmail.com>
2022-11-25 18:23:50 +0200

    add CSS styles for components

--- workShop/app/src/app/app.module.ts
+++ workShop/app/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { MainComponent } from './main/main.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeListComponent,
+    RecentPostsComponent,
+    MainComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
