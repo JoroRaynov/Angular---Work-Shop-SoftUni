7393fd32c09ee94686fda5333d859435766663de
Georgi Raynov <jornalo891@gmail.com>
2022-11-26 09:44:46 +0200

    add recent post component and api-url

--- workShop/app/src/app/api.service.ts
+++ workShop/app/src/app/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment'
 
+const apiURL = environment.apiUrl;
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loadThemes() {
+    return this.http.get(apiURL + '/themes');
+  }
+  // /posts?limit=5
+  loadPosts(limit?: number) {
+    return this.http.get(apiURL + `/posts?${limit ? `limit=${limit}` : ''}`);
+  }
 }
