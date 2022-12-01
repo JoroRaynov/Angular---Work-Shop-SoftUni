9ada0d40200dc0c54d1293c29cd15318d2c92ba5
Georgi Raynov <jornalo891@gmail.com>
2022-12-01 14:17:31 +0200

    add logout Component

--- workShop/app/src/app/auth/auth.module.ts
+++ workShop/app/src/app/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LogoutComponent
   ],
   imports: [
     CommonModule
